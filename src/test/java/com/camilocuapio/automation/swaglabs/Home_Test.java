72c48ff50147357917ceab6d8be97bf99be36424
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-02-16 18:17:09 -0600

    The before method is added

--- src/test/java/com/camilocuapio/automation/swaglabs/Home_Test.java
+++ src/test/java/com/camilocuapio/automation/swaglabs/Home_Test.java
@@ -1,4 +1,24 @@
 package com.camilocuapio.automation.swaglabs;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
 public class Home_Test {
+    private WebDriver driver;
+    HomePage homePage;
+
+    @Before
+    public void setUp(){
+        Base base=new Base();
+        driver=base.chromeDriverConnection();
+        if (driver !=null){
+            homePage=new HomePage(driver);
+            homePage.visit("https://www.saucedemo.com/");
+        }else {
+            throw new RuntimeException("The WebDriver could not be started. Check your Chrome version.");
+        }
+    }
+    //Close page
+    @Test
 }
